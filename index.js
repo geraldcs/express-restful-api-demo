50c37e81c4f686101ff171864212c26cd8cb24b7
Fritz Latagan <gerald.latagan@gmail.com>
2022-08-18 07:29:18 +0800

    Update index.js
    - added comments
    - added express.json()

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// 1. require in Express
 const express = require('express');
+// 2. create an Express application
 const app = express();
-
+// 2a. inform Express that we are going to use JSON for request and response
+app.use(express.json());
+// 3. define some routes
 app.get('/', function(req, res) {
     res.send('Sample home page');
 })
-
+// 4. start the server
 app.listen(4000, () => {
     console.log('server has started');
 })
